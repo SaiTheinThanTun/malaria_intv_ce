521e218f2e14238a117dbc3039fc031accd6d00d
SaiTheinThanTun <driftwood711@gmail.com>
2015-05-08 12:31:40 +0630

    x2013 changed to No.of.Test

--- CHW efficiency analysis_clean.R
+++ CHW efficiency analysis_clean.R
@@ -10,7 +10,7 @@ setwd("~/R/DataRep_report052015")
 rdt <- read.csv("rdt_longfile.csv")
 
 #checking variable names
-names(rdt) %in% c("State..Division","Township","TS_Pcode", "Volunteer", "Month","Year", "Source", "X2013")
+names(rdt) %in% c("State..Division","Township","TS_Pcode", "Volunteer", "Month","Year", "Source", "No.of.Test")
 #if the above name check turns out to include any FALSE, there's something wrong with the dataset
 
 #no. of Invalid IDs (blank Volunteer names...)
@@ -23,9 +23,9 @@ table(rdt$Month) #comparing RDTs per month
 ###RESHAPing before analying (instead of SPLIT). HOWEVER, melting doesn't change anything since the data is already in the long format
 library(reshape2)
 id_rdt <- names(rdt)
-m_rdt <- melt(rdt, id=id_rdt[-7]) #melting rdt into m_rdt. If the name at rdt[-7] is changed to variable/value, melting may not be required
+m_rdt <- melt(rdt, id=id_rdt[-8]) #melting rdt into m_rdt. If the name at rdt[-7] is changed to variable/value, melting may not be required
 
-#this Overall monthly average may not be necessary
+#this Overall monthly average may NOT be necessary
 dcast(m_rdt, Month ~ variable, mean) #Average RDTs per month in 2013
 
 mean((m_rdt$value)) #11.01737 tests per month Total average in 2013
@@ -37,20 +37,20 @@ table(TMrdt$Township) #sum(table(TMrdt$Township)>12) to check if any township is
 #Mean RDT tests per CHW
 rdt_uniq_vts_avg <- dcast(m_rdt, Volunteer+Township+Source ~ variable, mean) #Per VHW, the mean RDTs 
 
-median(rdt_uniq_vts_avg$X2013) #Median testing rate of 5 is a more robust measure since there are outstanding CHWs with >200 RDTs 
-sd(rdt_uniq_vts_avg$X2013) #15.28
+median(rdt_uniq_vts_avg$No.of.Test) #Median testing rate of 5 is a more robust measure since there are outstanding CHWs with >200 RDTs 
+sd(rdt_uniq_vts_avg$No.of.Test) #15.28
 
 #Bins from the first report (oct 2014)
-rdt_uniq_vts_avg$f <- cut(rdt_uniq_vts_avg$X2013, c(0,10,20,30,40,50,250), labels=c("<=10","11-20","21-30","31-40","41-50",">50"))
-barplot(table(rdt_uniq_vts_avg$f), main=paste("Average RDT testing rates of CHWs per month \n in 2013 (median=",median(rdt_uniq_vts_avg$X2013),")",sep=""), xlab= "No. of RDTs", ylab= "No. of CHWs")
+rdt_uniq_vts_avg$f <- cut(rdt_uniq_vts_avg$No.of.Test, c(0,10,20,30,40,50,250), labels=c("<=10","11-20","21-30","31-40","41-50",">50"))
+barplot(table(rdt_uniq_vts_avg$f), main=paste("Average RDT testing rates of CHWs per month \n in 2013 (median=",median(rdt_uniq_vts_avg$No.of.Test),")",sep=""), xlab= "No. of RDTs", ylab= "No. of CHWs")
 
 #experimental bins
 #base 2 bin
-rdt_uniq_vts_avg$f <- cut(rdt_uniq_vts_avg$X2013, c(0,2^(0:6)[-1],250))
-barplot(table(rdt_uniq_vts_avg$f), main=paste("Average RDT testing rates of CHWs per month \n in 2013 (median=",median(rdt_uniq_vts_avg$X2013),")",sep=""), xlab= "No. of RDTs", ylab= "No. of CHWs")
+rdt_uniq_vts_avg$f <- cut(rdt_uniq_vts_avg$No.of.Test, c(0,2^(0:6)[-1],250))
+barplot(table(rdt_uniq_vts_avg$f), main=paste("Average RDT testing rates of CHWs per month \n in 2013 (median=",median(rdt_uniq_vts_avg$No.of.Test),")",sep=""), xlab= "No. of RDTs", ylab= "No. of CHWs")
 
 #Per IP, preprocessed above for unique CHW with Volunteer name, Township and Source
-boxplot(X2013 ~ Source,rdt_uniq_vts_avg, xlab="Implementing partners", ylab="RDTs", main="RDT rates among Implementing Partners (2013)") #Boxplot comparing testing rates between IPs
+boxplot(No.of.Test ~ Source,rdt_uniq_vts_avg, xlab="Implementing partners", ylab="RDTs", main="RDT rates among Implementing Partners (2013)") #Boxplot comparing testing rates between IPs
 
 #Per township, need to have cleaned township names 
 rdt_uniq_t_avg <- dcast(m_rdt, Township ~ variable, mean)
