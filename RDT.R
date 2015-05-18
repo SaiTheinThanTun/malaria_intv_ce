83f0c62a07259b175ce4d485902553fc2266771b
SaiTheinThanTun <driftwood711@gmail.com>
2015-05-18 11:12:44 +0630

    Scripts for cleaning separated
    As it'd be redundant to correct the reading and cleaning scripts in TWO
    files, whenever there's an error, a new script file dedicated for the
    data management has been created.

--- RDT.R
+++ RDT.R
@@ -1,55 +1,7 @@
 #RDT testing and their results/ Positivity, etc
 #Analyzed from Access Query: qrtTC_combined +Type
+#Data reading and cleaning has to be done with a seperate script: RDT_reading_cleaning.R
 
-setwd("~/R/DataRep_report052015")
-
-
-#Setting up name vectors for Outcome
-pf <- c("Mix","pf","Pf","PF")
-npf <- c("Non-Pf","pv","Pv")
-neg <- "Neg"
-
-#rdt_org <- read.csv("TC_combined.csv")
-#rdt_org <- read.csv("TC_combined20150512.csv")
-rdt_org <- read.csv("marc2013TC_combined_20150513.csv")
-
-#checking names
-sum(names(rdt_org) %in% c("Expr1", "SR_Pcode","State_Region","Tsp_Code","Township","Yr","Mth", "Age_Group","Outcome","Diag_method","Number", "Source","Type"))==13
-#Changing the name of "SumOfNumber" variable into "Number"
-if(sum(names(rdt_org) %in% "SumOfNumber") >0){
-  names(rdt_org)[names(rdt_org)=="SumOfNumber"] <- "Number"
-}
-#Outcome table for Tom
-#ocTom <- dcast(rdt_org, Yr ~ Outcome, sum, na.rm=TRUE, value.var="Number")
-#write.csv(ocTom, "outcomes.csv")
-
-#cleaning up
-rdt <- rdt_org[rdt_org$Outcome %in% c(pf,npf,neg),] #46658 12 -> 66844 12
-rdt <- rdt[rdt$Mth!="",] #46132 12
-rdt <- rdt[!is.na(rdt$Yr),] #46132 12
-#Other filtering can also be done here, such as filtering for MARC region
-#Box link: https://app.box.com/s/4jwka23zsbsukxtng1c8t6jq9vpxayy1
-#Dropbox link: https://www.dropbox.com/s/wq1rdkdin9a5c4g/MARC%20PCodes.csv?dl=0
-
-#marc_p <- read.csv("E:\\Box Sync\\MOCRU\\Data\\MARC PCodes.csv") #Windows link
-marc_p <- read.csv("MARC PCodes.csv")
-marc_p <- marc_p$Tsp_Code #this changes the data.frame into a vector which we can use for subsetting.
-rdt <- rdt[rdt$Tsp_Code %in% marc_p,]
-
-
-
-rdt$Mth <- toupper(rdt$Mth)
-rdt$State_Region <- toupper(rdt$State_Region)
-rdt$Township <- toupper(rdt$Township)
-rdt$Mth <- factor(rdt$Mth, c("JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"))
-table(rdt$Mth)
-#rdt$Outcome <- factor(rdt$Outcome)
-
-#Recoding the Outcome variable
-rdt$Outcome[rdt$Outcome %in% pf] <- "Pf"
-rdt$Outcome[rdt$Outcome %in% npf] <- "Non-Pf"
-#rdt$Outcome[rdt$Outcome %in% neg] <- "Neg"
-rdt$Outcome <- factor(rdt$Outcome)
 
 #Required libraries for loading the function
 library(zoo)
--- RDT_barplot.R
+++ RDT_barplot.R
@@ -1,54 +1,8 @@
 #RDT testing and their results/ Positivity, etc
 #Analyzed from Access Query: qrtTC_combined +Type
-
-setwd("~/R/DataRep_report052015")
-
-
-#Setting up name vectors for Outcome
-pf <- c("Mix","pf","Pf","PF")
-npf <- c("Non-Pf","pv","Pv")
-neg <- "Neg"
-
-#rdt_org <- read.csv("TC_combined.csv")
-#rdt_org <- read.csv("TC_combined20150512.csv")
-rdt_org <- read.csv("marc2013TC_combined_20150513.csv")
-
-#Changing the name of "SumOfNumber" variable into "Number"
-if(sum(names(rdt_org) %in% "SumOfNumber") >0){
-  names(rdt_org)[names(rdt_org)=="SumOfNumber"] <- "Number"
-}
-#Outcome table for Tom
-#ocTom <- dcast(rdt_org, Yr ~ Outcome, sum, na.rm=TRUE, value.var="Number")
-#write.csv(ocTom, "outcomes.csv")
-
-#cleaning up
-rdt <- rdt_org[rdt_org$Outcome %in% c(pf,npf,neg),] #46658 12 -> 66844 12
-rdt <- rdt[rdt$Mth!="",] #46132 12
-rdt <- rdt[!is.na(rdt$Yr),] #46132 12
-#Other filtering can also be done here, such as filtering for MARC region
-#Box link: https://app.box.com/s/4jwka23zsbsukxtng1c8t6jq9vpxayy1
-#Dropbox link: https://www.dropbox.com/s/wq1rdkdin9a5c4g/MARC%20PCodes.csv?dl=0
-
-#marc_p <- read.csv("E:\\Box Sync\\MOCRU\\Data\\MARC PCodes.csv") #Windows link
-marc_p <- read.csv("MARC PCodes.csv")
-marc_p <- marc_p$Tsp_Code #this changes the data.frame into a vector which we can use for subsetting.
-rdt <- rdt[rdt$Tsp_Code %in% marc_p,]
-
+#Data reading and cleaning has to be done with a seperate script: RDT_reading_cleaning.R
 
 library(reshape2)
-rdt$Mth <- toupper(rdt$Mth)
-rdt$State_Region <- toupper(rdt$State_Region)
-rdt$Township <- toupper(rdt$Township)
-rdt$Mth <- factor(rdt$Mth, c("JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"))
-table(rdt$Mth)
-#rdt$Outcome <- factor(rdt$Outcome)
-
-#Recoding the Outcome variable
-rdt$Outcome[rdt$Outcome %in% pf] <- "Pf"
-rdt$Outcome[rdt$Outcome %in% npf] <- "Non-Pf"
-#rdt$Outcome[rdt$Outcome %in% neg] <- "Neg"
-rdt$Outcome <- factor(rdt$Outcome)
-
 combined <- dcast(rdt, Yr+Mth ~ Outcome, sum, na.rm=TRUE, value.var="Number") #To graph testing per month graphs
 #comb2013 <- combined[combined$Yr==2013,]
 
@@ -56,16 +10,18 @@ combined <- dcast(rdt, Yr+Mth ~ Outcome, sum, na.rm=TRUE, value.var="Number") #T
 tcomb <- t(combined[,-1])
 colnames(tcomb) <- tcomb[1,]
 tcomb <- tcomb[-1,]
+png(file=paste("stacked_oc_mnth_",Sys.Date(),".png",sep=""), width=850, height=480)
 barplot(tcomb, col=c("cornflowerblue","orange","coral1"), border="white", main="Malaria Outcomes per Month\n MARC region, 2013", ylab="No. of Malaria Outcomes")
-legend("topleft",legend=c("Negative","Non-Pf","Pf"),fill=c("cornflowerblue","orange","coral1"))
-
+legend("topleft",legend=c("Negative","Non-Pf","Pf"),fill=c("cornflowerblue","orange","coral1"), horiz=TRUE)
+dev.off()
 #percentage stacked plot per month
 
 tcomb_prop <- t(prop.table(as.matrix(combined[,3:5]),1))
 colnames(tcomb_prop) <- combined$Mth
+png(file=paste("stacked_oc_mnth_percent_",Sys.Date(),".png",sep=""), width=850, height=480)
 barplot(tcomb_prop, col=c("cornflowerblue","orange","coral1"), border="white", main="Percentage of Malaria Outcomes per Month\n MARC region, 2013", ylab="Percentage of Malaria Outcomes per Month")
 legend("bottomright",legend=c("Negative","Non-Pf","Pf"),fill=c("cornflowerblue","orange","coral1"))
-
+dev.off()
 
 #plot(combined$Neg[3:38] ~ combined$yrmth[3:38], type="s")
 
@@ -77,13 +33,18 @@ states <- dcast(rdt, State_Region ~ Outcome, sum, na.rm=TRUE, value.var="Number"
 tstates <- t(states)
 colnames(tstates) <- states$State_Region
 tstates <- tstates[-1,]
-barplot(tstates)
+png(file=paste("stacked_oc_states_",Sys.Date(),".png",sep=""), width=850, height=480)
+barplot(tstates, col=c("cornflowerblue","orange","coral1"), border="white", main="Malaria Outcomes per State/Division\n in MARC region, 2013", ylab="No. of Malaria Outcomes")
+legend("topleft",legend=c("Negative","Non-Pf","Pf"),fill=c("cornflowerblue","orange","coral1"), horiz=TRUE)
+dev.off()
 
 #percent-based stacked bars #pf npf comparison between states..divisons
 prop <- t(prop.table(as.matrix(states[,2:4]),1))
 colnames(prop) <- states$State_Region
+png(file=paste("stacked_oc_states_percent_",Sys.Date(),".png",sep=""), width=850, height=480)
 barplot(prop,col=c("cornflowerblue","orange","coral1"), border="white", main="Percentage of Malaria Outcomes in different States/Divisions\n MARC region, 2013", ylab="Percentage of Malaria Outcomes per State/Division")
 legend("bottomright",legend=c("Negative","Non-Pf","Pf"),fill=c("cornflowerblue","orange","coral1"))
+dev.off()
 
 #pf npf percentage comparison between townships
 tsp <- dcast(rdt, Township ~ Outcome, sum, na.rm=TRUE, value.var="Number")
